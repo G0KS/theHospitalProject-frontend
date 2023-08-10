2299c3d10ac51262b0d1841da195480bd5c31830
G0KS <goksrofficial@gmail.com>
2023-08-10 12:35:39 +0530

    new components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { OurDoctorsComponent } from './our-doctors/our-doctors.component';
+import { OurFacilitiesComponent } from './our-facilities/our-facilities.component';
+import { OurServicesComponent } from './our-services/our-services.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LandingComponent,
   },
-
-  
+  {
+    path: 'our-doctors',
+    component: OurDoctorsComponent,
+  },
+  {
+    path: 'our-facilities',
+    component: OurFacilitiesComponent,
+  },
+  {
+    path: 'our-services',
+    component: OurServicesComponent,
+  },
   {
     path: '**',
     component: PageNotFoundComponent,
